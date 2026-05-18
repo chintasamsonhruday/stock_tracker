c2b8507bc3b8874253759d4e9b747dbd8e34c27d
JontySam <jontysam15@gmail.com>
2026-05-18 17:21:37 -0500

    Add local launcher script

---
+++ Start-OpenStock.ps1
@@ -0,0 +1,50 @@
+$ErrorActionPreference = "Stop"
+
+$projectDir = Split-Path -Parent $MyInvocation.MyCommand.Path
+$chromeCandidates = @(
+    "$env:ProgramFiles\Google\Chrome\Application\chrome.exe",
+    "${env:ProgramFiles(x86)}\Google\Chrome\Application\chrome.exe",
+    "$env:LOCALAPPDATA\Google\Chrome\Application\chrome.exe"
+)
+$chrome = $chromeCandidates | Where-Object { Test-Path $_ } | Select-Object -First 1
+$url = "http://localhost:3000"
+
+$dockerReady = $false
+try {
+    docker info *> $null
+    $dockerReady = $LASTEXITCODE -eq 0
+} catch {
+    $dockerReady = $false
+}
+
+if (-not $dockerReady) {
+    $dockerDesktop = "$env:ProgramFiles\Docker\Docker\Docker Desktop.exe"
+    if (Test-Path $dockerDesktop) {
+        Start-Process -FilePath $dockerDesktop -WindowStyle Hidden
+    }
+
+    $deadline = (Get-Date).AddMinutes(3)
+    do {
+        Start-Sleep -Seconds 5
+        docker info *> $null
+        if ($LASTEXITCODE -eq 0) {
+            $dockerReady = $true
+            break
+        }
+    } while ((Get-Date) -lt $deadline)
+}
+
+if ($dockerReady) {
+    Push-Location $projectDir
+    try {
+        docker compose up -d
+    } finally {
+        Pop-Location
+    }
+}
+
+if ($chrome) {
+    Start-Process -FilePath $chrome -ArgumentList $url
+} else {
+    Start-Process $url
+}
